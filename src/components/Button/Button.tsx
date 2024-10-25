bd76fdcd47f6088e573431721ee2bddd617549fd
Sergii <dykyy.s@gmail.com>
2024-10-25 09:42:01 +0300

    add mr/ml

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,10 @@
 import { IButton } from './Button.types';
 
 export const Button = ({ children, onClick, position }: Readonly<IButton>) => {
-  console.log(position !== 'left' ? 'r' : 'l');
+  const side = position !== 'left' ? 'mr-2 ' : 'ml-2';
   return (
     <button
-      className={` absolute ${position}-0 top-1/2 z-10 m${
-        position !== 'left' ? 'r' : 'l'
-      }-2 h-8 w-8 hover:bg-yellow focus:bg-yellow hover:text-softCoral focus:text-softCoral rounded-full`}
+      className={`absolute ${position}-0 top-1/2 z-10 ${side} h-8 w-8 hover:bg-yellow focus:bg-yellow hover:text-softCoral focus:text-softCoral rounded-full`}
       onClick={onClick}
     >
       {children}
